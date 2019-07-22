57a125598846dda15771fc8d48b81b6b8e983672
Kenny <kawhyte@gmail.com>
2019-07-22 15:17:43 -0700

    This is just a test

--- src/components/landing/Projects/index.jsx
+++ src/components/landing/Projects/index.jsx
@@ -15,7 +15,7 @@ export const Projects = () => {
 	} = useStaticQuery(graphql`
 		{
 			github {
-				repositoryOwner(login: "smakosh") {
+				repositoryOwner(login: "kawhyte") {
 					repositories(
 						first: 8
 						orderBy: { field: STARGAZERS, direction: DESC }

fc618f02f6784c725ce352bd2e83f4e0579304f8
Kenny <kawhyte@gmail.com>
2019-12-09 09:11:30 -0800

    Added blog links

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -3,11 +3,11 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { Wrapper } from './styles'
 
 const NavbarLinks = ({ desktop }) => (
-	<Wrapper desktop={desktop}>
-		{/* <AnchorLink href="https://angry-saha-b0191c.netlify.com/">About Me</AnchorLink> */}
-		<AnchorLink href="#projects">Personal Projects</AnchorLink>
-		<AnchorLink href="#contact">Contact</AnchorLink>
-	</Wrapper>
+  <Wrapper desktop={desktop}>
+    <AnchorLink href="https://dev.to/kawhyte">Blog</AnchorLink>
+    <AnchorLink href="#projects">Personal Projects</AnchorLink>
+    <AnchorLink href="#contact">Contact</AnchorLink>
+  </Wrapper>
 )
 
 export default NavbarLinks

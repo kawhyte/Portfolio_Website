8a20f2d08e2712504841032fd76be878b3c669ed
Kenny <kawhyte@gmail.com>
2019-12-10 20:07:34 -0800

    Removed blog link

--- src/components/theme/Header/NavbarLinks/index.jsx
+++ src/components/theme/Header/NavbarLinks/index.jsx
@@ -4,7 +4,7 @@ import { Wrapper } from './styles'
 
 const NavbarLinks = ({ desktop }) => (
   <Wrapper desktop={desktop}>
-    <AnchorLink href="https://dev.to/kawhyte">Blog</AnchorLink>
+    {/* <AnchorLink href="https://dev.to/kawhyte">Blog</AnchorLink> */}
     <AnchorLink href="#projects">Personal Projects</AnchorLink>
     <AnchorLink href="#contact">Contact</AnchorLink>
   </Wrapper>

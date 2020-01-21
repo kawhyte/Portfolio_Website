1d8f64578c06aee4df7e110ffe880fdb1e45a690
Kenny <kawhyte@gmail.com>
2020-01-21 13:55:06 -0800

    SVG Update

--- src/components/theme/Footer/index.jsx
+++ src/components/theme/Footer/index.jsx
@@ -4,36 +4,39 @@ import { Wrapper, Flex, Links, Details } from './styles'
 import social from './social.json'
 
 export const Footer = () => (
-	<Wrapper>
-		<Flex as={Container}>
-			<Details>
-				<h2>Kenny Whyte</h2>
-				<span>
-					© All rights are reserved | {new Date().getFullYear()} | Original template done{' '}
-				
-					by{' '}
-					<a
-						href="https://smakosh.com/?ref=portfolio-dev"
-						rel="noopener noreferrer"
-						target="_blank"
-					>
-						Smakosh
-					</a>
-				</span>
-			</Details>
-			<Links>
-				{social.map(({ id, name, link, icon }) => (
-					<a
-						key={id}
-						href={link}
-						target="_blank"
-						rel="noopener noreferrer"
-						aria-label={`follow me on ${icon}`}
-					>
-						<img width="24" src={icon} alt={name} />
-					</a>
-				))}
-			</Links>
-		</Flex>
-	</Wrapper>
+  <Wrapper>
+    <Flex as={Container}>
+      <Details>
+        <h2>Kenny Whyte</h2>
+        <span>
+          © All rights are reserved | {new Date().getFullYear()} | Original
+          template done by{' '}
+          <a
+            href="https://smakosh.com/?ref=portfolio-dev"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            Smakosh
+          </a>{' '}
+          | Business vector created by freepik -{' '}
+          <a href="https://www.freepik.com/free-photos-vectors/business">
+            www.freepik.com
+          </a>
+        </span>
+      </Details>
+      <Links>
+        {social.map(({ id, name, link, icon }) => (
+          <a
+            key={id}
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label={`follow me on ${icon}`}
+          >
+            <img width="24" src={icon} alt={name} />
+          </a>
+        ))}
+      </Links>
+    </Flex>
+  </Wrapper>
 )
